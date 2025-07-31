e23094a15e4040f30d1797be6444487c57969715
rohitkhodave <khodaverohit1@gmail.com>
2025-07-31 13:14:07 +0530

    rohit

---
+++ arrayexample.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class arrayexample
+{
+public static void main(String args []){
+int sum = 0;
+int arr[]={10,45,67,10,45,7};
+for(int i=0; i<arr.length ;i++){
+System.out.println(arr[i]);
+sum = sum +arr[i];
+}
+System.out.println("the sum of arrray is"+sum);
+}
+}
