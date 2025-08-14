608466d1c4eedee433b3b42d04eeda22fb2847c4
pranali <kokatepranu833@gmail.com>
2025-08-14 11:51:09 +0530

    rohit

---
+++ Scannerclass.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Scannerclass{
+public static void main(String args []){
+Scanner obj=new Scanner(System.in);
+System.out.println("enter a boolean value");
+boolean data = obj.nextBoolean();
+System.out.println("your name is "+data);
+}
+}
+ 
---
+++ Scannerclassexample.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Scannerclassexample{
+public static void main(String args[]){
+Scanner obj =new Scanner(System.in);
+System.out.println("enter a name");
+String userName =obj.next();
+System.out.println("your name is "+userName);
+}
+}
